32e8cc5b69ea8a88f8d8b8a7910e69d4ba064b0c
abi97 <abi97@DESKTOP-QL0K8G8>
2023-12-28 12:07:11 +0530

    Add project files.

---
+++ ASP.NET Core Web API/Data/AspNetCoreWebApiDbContext.cs
@@ -0,0 +1,18 @@
+﻿using ASP.NET_Core_Web_API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASP.NET_Core_Web_API.Data
+{
+    public class AspNetCoreWebApi : DbContext
+    {
+        public AspNetCoreWebApi(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Difficulty> difficulties { get; set; }
+
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+    }
+}
---
+++ ASP.NET Core Web API/Models/Domain/Region.cs
@@ -0,0 +1,10 @@
+﻿namespace ASP.NET_Core_Web_API.Models.Domain
+{
+    public class Region
+    {
+        public Guid ID { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string? RegionImageUrl { get; set; }
+    }
+}
